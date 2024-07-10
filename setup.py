e8501058ca8deea5da7836c4661274e30d50c306
cgalelli <claudio.galelli@obspm.fr>
2024-07-10 17:23:59 +0200

    Inital commit of functions and setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="gammapy_SyLC",
+    version="0.1.0",
+    description="A tool for synthetic lightcurves for Gammapy",
+    package_dir={"": "gammapy_SyLC"},
+    packages=find_packages(where="gammapy_SyLC"),
+    author="Claudio Galelli",
+    author_email="<claudio.galelli@obspm.fr>",
+    license="BSD 3-Clause",
+    repository="https://github.com/cgalelli/gammapy_SyLC.git",
+    readme="README.md",
+    classifiers=["Topic :: Scientific/Engineering"],
+    install_requires=["numpy>=2.0", "gammapy>=1.2", "scipy>=1.13",],
+    python_requires=">3.10",
+)
