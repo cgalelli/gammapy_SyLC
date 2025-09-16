93351830bb12ebb262d035b955956dae3b0749ea
cgalelli <claudio.galelli@mi.infn.it>
2025-09-16 12:06:09 +0200

    Correct init

--- gammapy_SyLC/__init__.py
+++ gammapy_SyLC/__init__.py
@@ -16,7 +16,7 @@ from .fitting import (
     compare_models,
 )
 
-from .multiwavelength import calculate_zdcf, simulate_lightcurve_from_reference, mwl_psd_envelope
+from .multiwavelength import calculate_zdcf
 
 from .helpers import lightcurve_psd_envelope, interp_pdf
 
